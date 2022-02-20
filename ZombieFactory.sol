a40b147bd78932890f25bedae618cdca782b01ce
Esfekia <sertac@gmail.com>
2022-02-20 10:22:58 -0500

    Lesson 1 Chapter 4

---
+++ ZombieFactory.sol
@@ -0,0 +1,9 @@
+//Chapter 4: Create a uint named dnaModulus, and set it equal to 10 to the power of dnaDigits.
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    //start here
+    uint dnaModulus = 10 ** dnaDigits;
+}
