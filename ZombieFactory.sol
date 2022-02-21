ff3ff1a2578b75b7936889f87f1669a5eb19e600
esfekia <sertac@gmail.com>
2022-02-21 18:03:37 -0500

    Chapter 7 - create public function createZombie

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -44,3 +44,26 @@ contract ZombieFactory {
     // start here
 
 }
+
+//Chapter 7 Create a public function named createZombie. It should take two parameters: _name (a string), and _dna (a uint). 
+//Don't forget to pass the first argument by value by using the memory keyword
+
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    // start here
+    function createZombie(string memory _name, uint _dna) public {
+        
+    }
+}
