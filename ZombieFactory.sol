6737052de61153a3775f3f12038059ed96affced
Esfekia <sertac@gmail.com>
2022-02-21 16:33:20 -0500

    Public Array of Zombies added.

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -25,3 +25,22 @@ contract ZombieFactory {
         uint dna;
     }
 }
+
+//Chapter 6: Create a public array of Zombies:
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    // start here
+
+}
