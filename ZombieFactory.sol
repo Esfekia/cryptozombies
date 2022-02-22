34ab22d333244c83bf02ec60f0fac02c54637743
esfekia <sertac@gmail.com>
2022-02-22 06:26:15 -0500

    Chapter 13: Event NewZombie added to our code.

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -166,3 +166,82 @@ contract ZombieFactory {
         return rand % dnaModulus;
     }
 }
+
+//Chapter 12: Put it all together and call the functions in a function named createRandomZombie
+
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+    uint256 dnaDigits = 16;
+    uint256 dnaModulus = 10**dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint256 dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string memory _name, uint256 _dna) private {
+        zombies.push(Zombie(_name, _dna));
+    }
+
+    function _generateRandomDna(string memory _str)
+        private
+        view
+        returns (uint256)
+    {
+        uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    // start here
+    function createRandomZombie(string memory _name) public {
+        uint256 randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
+
+//Chapter 13: Events
+//We want an event to let our front-end know every time a new zombie was created, so the app can display it.
+//Declare an event called NewZombie. It should pass zombieId (a uint), name (a string), and dna (a uint).
+//Modify the _createZombie function to fire the NewZombie event after adding the new Zombie to our zombies array.
+//You're going to need the zombie's id. array.push() returns a uint of the new length of the array -
+//and since the first item in an array has index 0, array.push() - 1 will be the index of the zombie we just added.
+//Store the result of zombies.push() - 1 in a uint called id, so you can use this in the NewZombie event in the next line.
+
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+    // declare our event here
+    event NewZombie(uint256 zombieId, string name, uint256 dna);
+    uint256 dnaDigits = 16;
+    uint256 dnaModulus = 10**dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint256 dna;
+    }
+
+    Zombie[] public zombies;
+
+    function _createZombie(string memory _name, uint256 _dna) private {
+        uint256 id = zombies.push(Zombie(_name, _dna)) - 1;
+        // and fire it here
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str)
+        private
+        view
+        returns (uint256)
+    {
+        uint256 rand = uint256(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        uint256 randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
