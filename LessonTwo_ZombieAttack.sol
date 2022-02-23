e75fc6dfa0134068cad8168d4220ba35a9e3f802
Esfekia <sertac@gmail.com>
2022-02-23 11:51:37 -0500

    Chapter 2.4 require example added.

--- LessonTwo_ZombieAttack.sol
+++ LessonTwo_ZombieAttack.sol
@@ -86,3 +86,48 @@ contract ZombieFactory {
     }
 
 }
+
+//Chapter 4: Require
+//Let's use require to make sure this function only gets executed one time per user, when they create their first zombie.
+//Put a require statement at the beginning of createRandomZombie. 
+//The function should check to make sure ownerZombieCount[msg.sender] is equal to 0, and throw an error otherwise.
+
+pragma solidity >=0.5.0 <0.6.0;
+
+contract ZombieFactory {
+
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string memory _name, uint _dna) private {
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+        emit NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string memory _str) private view returns (uint) {
+        uint rand = uint(keccak256(abi.encodePacked(_str)));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string memory _name) public {
+        // start here
+        require(ownerZombieCount[msg.sender] == 0);
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+
+}
