1a265f8092ef48436a54a6e4d8c7047344efe3ce
Esfekia <sertac@gmail.com>
2022-02-23 12:43:45 -0500

    zombiefeeding updated with commends for chapter 2.7

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -5,3 +5,19 @@ import "./zombiefactory.sol";
 contract ZombieFeeding is ZombieFactory {
 
 }
+
+//Chapter 2.7 Storage vs Memory
+When a zombie feeds on some other lifeform, its DNA will combine with the other lifeform's DNA to create a new zombie.
+
+/**Create a function called feedAndMultiply. It will take two parameters: _zombieId (a uint) and _targetDna (also a uint). This function should be public.
+
+We don't want to let someone else feed our zombie! So first, let's make sure we own this zombie. 
+Add a require statement to verify that msg.sender is equal to this zombie's owner (similar to how we did in the createRandomZombie function).
+
+Again, because our answer-checker is primitive, it's expecting msg.sender to come first and will mark it wrong if you switch the order. 
+But normally when you're coding, you can use whichever order you prefer — both are correct.
+
+We're going to need to get this zombie's DNA. 
+So the next thing our function should do is declare a local Zombie named myZombie (which will be a storage pointer). 
+Set this variable to be equal to index _zombieId in our zombies array.
+/*
