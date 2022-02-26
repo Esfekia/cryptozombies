42e172f9453de43e999743b5589e99de21e784dd
esfekia <sertac@gmail.com>
2022-02-26 05:34:20 -0500

    Chapter 2.12 Handling Multiple Values, function feedOnKitty added.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -20,8 +20,7 @@ contract KittyInterface {
 contract ZombieFeeding is ZombieFactory {
 
   address ckAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
-  // Initialize kittyContract here using `ckAddress` from above
-  KittyInterface kittyContract =KittyInterface(ckAddress);
+  KittyInterface kittyContract = KittyInterface(ckAddress);
 
   function feedAndMultiply(uint _zombieId, uint _targetDna) public {
     require(msg.sender == zombieToOwner[_zombieId]);
@@ -29,6 +28,13 @@ contract ZombieFeeding is ZombieFactory {
     _targetDna = _targetDna % dnaModulus;
     uint newDna = (myZombie.dna + _targetDna) / 2;
     _createZombie("NoName", newDna);
+  }
+
+  // define function here
+  function feedOnKitty(uint _zombieId, uint _kittyId) public {
+    uint kittyDna;
+    (,,,,,,,,,kittyDna) = kittyContract.getKitty(_kittyId);
+    feedAndMultiply(_zombieId, kittyDna);
   }
 
 }
@@ -60,3 +66,16 @@ Inside the interface, define the function getKitty (which should be a copy/paste
 
 I've saved the address of the CryptoKitties contract in the code for you, under a variable named ckAddress. 
 In the next line, create a KittyInterface named kittyContract, and initialize it with ckAddress — just like we did with numberContract above.
+
+//Chapter 2.12: Handling Multiple Return Values
+Let's make a function that gets the kitty genes from the contract:
+
+Make a function called feedOnKitty. It will take 2 uint parameters, _zombieId and _kittyId, and should be a public function.
+
+The function should first declare a uint named kittyDna.
+
+//Note: In our KittyInterface, genes is a uint256 — but if you remember back to lesson 1, uint is an alias for uint256 — they're the same thing.
+
+The function should then call the kittyContract.getKitty function with _kittyId and store genes in kittyDna. Remember — getKitty returns a ton of variables. (10 to be exact — I'm nice, I counted them for you!). But all we care about is the last one, genes. Count your commas carefully!
+
+Finally, the function should call feedAndMultiply, and pass it both _zombieId and kittyDna.
