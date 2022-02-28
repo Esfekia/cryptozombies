875b4895db86de040e5d29afc629cda12cdeb643
esfekia <sertac@gmail.com>
2022-02-28 05:38:42 -0500

    Chapter 3.4 Struct Packing - added two new traits to Zombie struct.

--- ZombieFactory.sol
+++ ZombieFactory.sol
@@ -13,6 +13,9 @@ contract ZombieFactory is Ownable {
     struct Zombie {
         string name;
         uint256 dna;
+        // Add new data here
+        uint32 level;
+        uint32 readyTime;
     }
 
     Zombie[] public zombies;
@@ -50,3 +53,11 @@ contract ZombieFactory is Ownable {
 //Modify our code to import the contents of ownable.sol. If you don't remember how to do this take a look at zombiefeeding.sol.
 
 //Modify the ZombieFactory contract to inherit from Ownable. Again, you can take a look at zombiefeeding.sol if you don't remember how this is done.
+
+//Chapter 3.4 Struct Packing:
+//In this lesson, we're going to add 2 new features to our zombies: level and readyTime — the latter will be used to implement a cooldown timer to limit how often a zombie can feed.
+
+//So let's jump back to zombiefactory.sol.
+
+//Add two more properties to our Zombie struct: level (a uint32), and readyTime (also a uint32). We want to pack these data types together, so let's put them at the end of the struct.
+//32 bits is more than enough to hold the zombie's level and timestamp, so this will save us some gas costs by packing the data more tightly than using a regular uint (256-bits).
