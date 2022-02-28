7e9be48c9515fcb96b8575c11151931b5015cb37
esfekia <sertac@gmail.com>
2022-02-28 06:19:33 -0500

    Chapter 3.6 Passing Structs as Arguments, _zombie used by _triggerCooldown and _isReady functions.

--- zombiefeeding.sol
+++ zombiefeeding.sol
@@ -21,9 +21,18 @@ contract ZombieFeeding is ZombieFactory {
 
   KittyInterface kittyContract;
 
-  // Modify this function:
   function setKittyContractAddress(address _address) external onlyOwner {
     kittyContract = KittyInterface(_address);
+  }
+
+  // 1. Define `_triggerCooldown` function here
+  function _triggerCooldown(Zombie storage _zombie) internal{
+      _zombie.readyTime = uint32(now + cooldownTime);
+  }
+
+  // 2. Define `_isReady` function here
+  function _isReady(Zombie storage _zombie)internal view returns(bool){
+      return (_zombie.readyTime <= now);
   }
 
   function feedAndMultiply(uint _zombieId, uint _targetDna, string memory _species) public {
@@ -44,7 +53,6 @@ contract ZombieFeeding is ZombieFactory {
   }
 
 }
-
 
 
 //Chapter 2.7 Storage vs Memory
@@ -113,3 +121,12 @@ Inside the function, add one line of code that sets kittyContract equal to Kitty
 //Now we can restrict access to setKittyContractAddress so that no one but us can modify it in the future.
 
 //Add the onlyOwner modifier to setKittyContractAddress.
+
+Chapter 3.6 Passing Structs as Arguments:
+Start by defining a _triggerCooldown function. It will take 1 argument, _zombie, a Zombie storage pointer. The function should be internal.
+
+The function body should set _zombie.readyTime to uint32(now + cooldownTime).
+
+Next, create a function called _isReady. This function will also take a Zombie storage argument named _zombie. It will be an internal view function, and return a bool.
+
+The function body should return (_zombie.readyTime <= now), which will evaluate to either true or false. This function will tell us if enough time has passed since the last time the zombie fed.
