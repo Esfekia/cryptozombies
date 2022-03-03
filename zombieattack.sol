7e954bc1aa940c3b88f6be7da95f56b3e7dff7a5
esfekia <sertac@gmail.com>
2022-03-03 06:12:13 -0500

    Chapter 4.4 attack function and attackVictoryProbability added.

--- zombieattack.sol
+++ zombieattack.sol
@@ -3,14 +3,21 @@ pragma solidity >=0.5.0 <0.6.0;
 import "./zombiehelper.sol";
 
 contract ZombieAttack is ZombieHelper {
-  // Start here
   uint randNonce = 0;
+  // Create attackVictoryProbability here
+  uint attackVictoryProbability = 70;
+
   function randMod(uint _modulus) internal returns(uint) {
-      randNonce++;
-      return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
+    randNonce++;
+    return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
   }
 
+  // Create new function here
+  function attack(uint _zombieId, uint _targetId) external {
+      
+  }
 }
+
 
 
 
@@ -22,3 +29,8 @@ Create a function called randMod (random-modulus). It will be an internal functi
 The function should first increment randNonce (using the syntax randNonce++).
 Finally, it should (in one line of code) calculate the uint typecast of the keccak256 hash of abi.encodePacked(now,msg.sender,randNonce) — and return that value % _modulus. 
 (Whew! That was a mouthful. If you didn't follow that, just take a look at the example above where we generated a random number — the logic is very similar).
+
+Chapter 4.5 Zombie Fightin'
+Give our contract a uint variable called attackVictoryProbability, and set it equal to 70.
+Create a function called attack. It will take two parameters: _zombieId (a uint) and _targetId (also a uint). It should be an external function.
+Leave the function body empty for now.
