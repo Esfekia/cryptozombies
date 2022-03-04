7ccd6596d515b117afaf5b7a9a8d957f54b7ef76
Esfekia <sertac@gmail.com>
2022-03-04 06:22:19 -0500

    Chapter 4.8 ownerOf modifier added to attack function, also myZombie and enemyZombie is defined, randMod with 100 called and stored in rand.

--- zombieattack.sol
+++ zombieattack.sol
@@ -4,20 +4,22 @@ import "./zombiehelper.sol";
 
 contract ZombieAttack is ZombieHelper {
   uint randNonce = 0;
-  // Create attackVictoryProbability here
   uint attackVictoryProbability = 70;
 
   function randMod(uint _modulus) internal returns(uint) {
     randNonce++;
     return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
+
   }
 
-  // Create new function here
-  function attack(uint _zombieId, uint _targetId) external {
-      
+  // 1. Add modifier here
+    function attack(uint _zombieId, uint _targetId) external ownerOf(_zombieId) {
+    // 2. Start function definition here
+    Zombie storage myZombie = zombies[_zombieId];
+    Zombie storage enemyZombie = zombies[_targetId];
+    uint rand = randMod(100);
   }
 }
-
 
 
 
@@ -34,3 +36,11 @@ Chapter 4.5 Zombie Fightin'
 Give our contract a uint variable called attackVictoryProbability, and set it equal to 70.
 Create a function called attack. It will take two parameters: _zombieId (a uint) and _targetId (also a uint). It should be an external function.
 Leave the function body empty for now.
+
+Chapter 4.8 modifier in zombieattack
+Add the ownerOf modifier to attack to make sure the caller owns _zombieId.
+The first thing our function should do is get a storage pointer to both zombies so we can more easily interact with them:
+a. Declare a Zombie storage named myZombie, and set it equal to zombies[_zombieId].
+b. Declare a Zombie storage named enemyZombie, and set it equal to zombies[_targetId].
+We're going to use a random number between 0 and 99 to determine the outcome of our battle. 
+So declare a uint named rand, and set it equal to the result of the randMod function with 100 as an argument.
