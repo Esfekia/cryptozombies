c4695e8b53d11e1c8c396bbd539ec1098f81600f
Esfekia <sertac@gmail.com>
2022-03-05 12:16:56 -0500

    Chapter 5.5: added _transfer function to enable transfer of zombie ERC721 token

--- zombieownership.sol
+++ zombieownership.sol
@@ -6,13 +6,19 @@ import "./erc721.sol";
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
   function balanceOf(address _owner) external view returns (uint256) {
-    // 1. Return the number of zombies `_owner` has here
     return ownerZombieCount[_owner];
   }
 
   function ownerOf(uint256 _tokenId) external view returns (address) {
-    // 2. Return the owner of `_tokenId` here
     return zombieToOwner[_tokenId];
+  }
+
+  // Define _transfer() here
+  function _transfer(address _from, address _to, uint256 _tokenId) private {
+    ownerZombieCount[_to]++;
+    ownerZombieCount[_from]--;
+    zombieToOwner[_tokenId] = _to;
+    emit Transfer(_from, _to, _tokenId);
   }
 
   function transferFrom(address _from, address _to, uint256 _tokenId) external payable {
@@ -45,6 +51,27 @@ Take a look at our code from previous lessons to see where we're storing this da
 If you can't figure it out, you can hit the "show me the answer" button for some help.
 Implement balanceOf to return the number of zombies _owner has.
 Implement ownerOf to return the address of whoever owns the zombie with ID _tokenId.
+
+Chapter 5.5:
+Let's define the logic for _transfer.
+
+Define a function named _transfer. It will take 3 arguments, address _from, address _to, and uint256 _tokenId. 
+It should be a private function.
+
+We have 2 mappings that will change when ownership changes: ownerZombieCount 
+(which keeps track of how many zombies an owner has) and zombieToOwner (which keeps track of who owns what).
+
+The first thing our function should do is increment ownerZombieCount for the person receiving the zombie (address _to). 
+Use ++ to increment.
+
+Next, we'll need to decrease the ownerZombieCount for the person sending the zombie (address _from). Use -- to decrement.
+
+Lastly, we'll want to change zombieToOwner mapping for this _tokenId so it now points to _to.
+
+I lied, that wasn't the last step. There's one more thing we should do.
+
+The ERC721 spec includes a Transfer event. The last line of this function should fire Transfer with the correct information 
+— check erc721.sol to see what arguments it's expecting to be called with and implement it here.
 
 contract ERC721 {
   event Transfer(address indexed _from, address indexed _to, uint256 indexed _tokenId);
